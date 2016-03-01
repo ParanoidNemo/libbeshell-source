128f080194659958f04f76c8665f4a6535461470
ParanoidNemo <paranoid.nemo@gmail.com>
2016-03-01 12:48:26 +0100

    create theme handler

---
+++ theme.h
@@ -0,0 +1,15 @@
+#ifndef LIBBESHELL_H_INCLUDE
+#define LIBBESHELL_H_INCLUDE
+
+using namespace std;
+
+class Theme
+{
+public:
+    Theme();
+private:
+    string name;
+    string path;
+};
+
+#endif //LIBBESHELL_H_INCLUDE
